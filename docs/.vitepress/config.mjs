30926f80c1b9c7fbdce68e3403581517959cc06f
Kitty-0512 <Kitty-0512@users.noreply.github.com>
2026-08-04 22:06:33 +0800

    add bing meta verification

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -3,6 +3,9 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: 'Kitty Blog',
   description: 'Java / Spring Boot / Vue 技术笔记',
+  head: [
+    ['meta', { name: 'msvalidate.01', content: '91B7399AE12261064EE40FC425A3B381' }],
+  ],
   sitemap: {
     hostname: 'https://Kitty-0512.github.io',
   },
